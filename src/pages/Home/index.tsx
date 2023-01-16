6b2b3b5eb734baae44c3b9bef143667f6ff757e5
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-01-16 16:31:00 -0300

    trabalhando no fetch de dados via axios

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from "react";
+import { api } from "../../services/api";
+import { Movie } from "../../types/Movie";
+
 export function Home() {
+	const [listMoviesShowing, setListMoviesShowing] = useState<Movie[]>([]);
+
+	useEffect(() => {
+		async function loadMoviesShowing() {
+			const response = await api.get('movie/now_playing', {
+				params: {
+					api_key: import.meta.env.VITE_API_KEY,
+					language: 'pt-BR',
+					page: 1
+				}
+			});
+			setListMoviesShowing(response.data.results.slice(0, 10));
+
+		}
+
+		loadMoviesShowing();
+	}, []);
+
 	return (
-		<div>
-			<h1>Bem-vindo à Home!</h1>
-		</div>
+		<main className="container">
+			<div className="movie-list">
+				{listMoviesShowing.map(movie => (
+					<p>{movie.title}</p>
+				))}
+			</div>
+		</main>
 	);
 }

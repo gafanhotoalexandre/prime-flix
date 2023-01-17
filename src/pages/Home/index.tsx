559d2d4a68959adea2e40f17c62ae9cc46885dc5
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-01-17 00:01:55 -0300

    listando filmes e apresentando feedback de loading

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,51 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { api } from "../../services/api";
 import { Movie } from "../../types/Movie";
 
+import './Home.css';
+
 export function Home() {
 	const [listMoviesShowing, setListMoviesShowing] = useState<Movie[]>([]);
+	const [isLoadingMovies, setIsLoadingMovies] = useState(true);
 
 	useEffect(() => {
 		async function loadMoviesShowing() {
-			const response = await api.get('movie/now_playing', {
-				params: {
-					api_key: import.meta.env.VITE_API_KEY,
-					language: 'pt-BR',
-					page: 1
-				}
-			});
-			setListMoviesShowing(response.data.results.slice(0, 10));
-
+			setIsLoadingMovies(true);
+			try {
+				const response = await api.get('movie/now_playing', {
+					params: {
+						api_key: import.meta.env.VITE_API_KEY,
+						language: 'pt-BR',
+						page: 1
+					}
+				});
+				setListMoviesShowing(response.data.results.slice(0, 10));
+				setIsLoadingMovies(false);
+			} catch (error) {
+				console.warn(error)
+			}
 		}
-
 		loadMoviesShowing();
 	}, []);
+
+	if (isLoadingMovies) {
+		return (
+			<div className="loading">
+				<h2>Carregando...</h2>
+			</div>
+		);
+	}
 
 	return (
 		<main className="container">
 			<div className="movie-list">
 				{listMoviesShowing.map(movie => (
-					<p>{movie.title}</p>
+					<article key={movie.id}>
+						<strong>{movie.title}</strong>
+						<img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
+						<Link to={`/filme/${movie.id}`}>Acessar</Link>
+					</article>
 				))}
 			</div>
 		</main>

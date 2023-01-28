80799c81bfb5d32ed60a0410d7acdc972969b87e
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-01-28 10:31:03 -0300

    not found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
+import './NotFound.css';
+
 export function NotFound() {
 	return (
-		<div>
+		<div className="not-found">
+			<h1>404</h1>
 			<h2>Página não encontrada...</h2>
+			<Link to="/">Veja todos os filmes!</Link>
 		</div>
 	);
 }

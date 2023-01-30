aab7d10cda0f981212a01525a3c999573a53d5d8
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-01-30 14:57:20 -0300

    pagina de favoritos

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { MovieType } from "../../types/Movie";
+
+import './Favorites.css';
+
 export function Favorites() {
+	const [favoriteMovies, setFavoriteMovies] = useState<MovieType[]>();
+
+	useEffect(() => {
+		const myMovieList = localStorage.getItem('@primeFlix');
+		setFavoriteMovies(JSON.parse(String(myMovieList)) || [])
+	}, []);
+
 	return (
-		<div>
-			<h1>Favoritos</h1>
+		<div className="my-movies">
+			<h1>Meus Filmes</h1>
+
+			<ul>
+				{favoriteMovies?.map(movie => (
+					<li key={movie.id}>
+						<span>{movie.title}</span>
+
+						<div>
+							<Link to={`/filme/${movie.id}`}>Ver detalhes</Link>
+							<button>Remover</button>
+						</div>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
